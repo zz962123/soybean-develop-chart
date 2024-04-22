666691620548615981e6dc6fd9351b9f0f50ba3f
zz962123 <zz962126@gmail.com>
2024-04-22 16:59:08 +0900

    [dev] next install and font

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+export default function About() {
+    return <div>
+        <Head>
+            <title>About Page</title>
+        </Head>
+        About
+    </div>
+}
