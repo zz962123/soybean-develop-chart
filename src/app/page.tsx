1231aa5780eff088e31a62434eb594ae80aeadd9
CH <zz962126@gmail.com>
2024-04-23 23:49:07 +0900

    [dev] font css and next localFont

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>    
-      <main className={styles.main}>
-        MAIN
-      </main>
-    </>
+    <main className={styles.main}>
+      MAIN
+    </main>
   );
 }
--- src/utiles/fonts/fonts.ts
+++ src/utiles/fonts/fonts.ts
@@ -5,10 +5,10 @@ export const pretendard_light = localFont({
     variable: '--font-pretendard-light',
 });
 
-export const pretendard_regular = localFont({
-    src: './Pretendard-Regular.otf',
-    variable: '--font-pretendard-regular'
-});
+// export const pretendard_regular = localFont({
+//     src: './Pretendard-Regular.otf',
+//     variable: '--font-pretendard-regular'
+// });
 
 export const pretendard_semibold = localFont({
     src: './Pretendard-SemiBold.otf',
