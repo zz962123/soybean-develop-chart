8823fe8a012adea8a2931a42af624e14de26e9d5
zz962123 <zz962126@gmail.com>
2024-04-23 13:15:46 +0900

    [dev] next font Optomizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <>    
       <main className={styles.main}>
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,700&display=swap" rel="stylesheet" />
-          
         MAIN
       </main>
     </>
---
+++ src/utiles/fonts/fonts.ts
@@ -0,0 +1,16 @@
+import localFont from 'next/font/local'
+
+export const pretendard_light = localFont({
+    src: './Pretendard-Light.otf',
+    variable: '--font-pretendard-light',
+});
+
+export const pretendard_regular = localFont({
+    src: './Pretendard-Regular.otf',
+    variable: '--font-pretendard-regular'
+});
+
+export const pretendard_semibold = localFont({
+    src: './Pretendard-SemiBold.otf',
+    variable: '--font-pretendard-semibold'
+});
